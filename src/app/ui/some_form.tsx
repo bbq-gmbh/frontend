ad75dda2462779bc68857f25d386b0031538964b
# <e.0xcc3@gmail.com>
2025-09-12 16:49:41 +0200

    Refactor CreateUserForm component structure for improved readability

--- src/app/ui/some_form.tsx
+++ src/app/ui/some_form.tsx
@@ -8,28 +8,28 @@ export function CreateUserForm() {
   const [state, action, pending] = useActionState(createUser, undefined);
 
   return (
-    <form action={action} className="max-w-100 space-y-2 my-4">
-      <div className="grid grid-cols-2">
-        <label htmlFor="name">Name</label>
-        <input id="name" name="name" placeholder="Name" />
+    <>
+      <form action={action} className="max-w-100 space-y-2 my-4">
+        <div className="grid grid-cols-2">
+          <label htmlFor="name">Name</label>
+          <input id="name" name="name" placeholder="Name" />
+        </div>
+        <div className="grid grid-cols-2">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            placeholder="1234"
+            type="password"
+          />
+        </div>
+        <Button disabled={pending} type="submit">
+          Sign Up
+        </Button>
+      </form>
+      <div className={state?.error ? "text-red-500" : "text-green-400"}>
+        {state && <p className="break-all">{state.message}</p>}
       </div>
-      <div className="grid grid-cols-2">
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          name="password"
-          placeholder="1234"
-          type="password"
-        />
-      </div>
-      <Button disabled={pending} type="submit">
-        Sign Up
-      </Button>
-      {state && (
-        <p className={state.error ? "text-red-500" : "text-green-400"}>
-          {state.message}
-        </p>
-      )}
-    </form>
+    </>
   );
 }
