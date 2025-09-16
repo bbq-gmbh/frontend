61ae542e670390869d0cdf0c614475ba985ca71c
# <e.0xcc3@gmail.com>
2025-09-16 16:01:20 +0200

    testing

--- src/lib/backend.ts
+++ src/lib/backend.ts
@@ -1,14 +1,42 @@
+import * as sdk from "@/backend/sdk.gen";
+import * as types from "@/backend/types.gen";
+import { client } from "@/backend/client.gen";
+import { createConfig } from "@/backend/client";
+
 const backendUri = process.env.BACKEND;
 
 export function api(path: string): string {
-    if (backendUri === undefined) throw Error("Backend uri not defined in environment");
+  client.setConfig(
+    createConfig({
+      baseUrl: "http://localhost:3001",
+    })
+  );
 
-    try {
-        const base = backendUri.startsWith('http://') || backendUri.startsWith('https://')
-            ? backendUri
-            : `http://${backendUri}`;
-        return new URL(path, base).href;
-    } catch (e) {
-        throw new Error(`Invalid backend URI or path: ${e}`);
-    }
+  sdk.createUserUsersPost({
+    body: {
+      username: "abcd",
+      password: "pswd",
+    },
+  });
+
+  const token = "";
+
+  sdk.refreshTokenAuthRefreshPost({
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (backendUri === undefined)
+    throw Error("Backend uri not defined in environment");
+
+  try {
+    const base =
+      backendUri.startsWith("http://") || backendUri.startsWith("https://")
+        ? backendUri
+        : `http://${backendUri}`;
+    return new URL(path, base).href;
+  } catch (e) {
+    throw new Error(`Invalid backend URI or path: ${e}`);
+  }
 }
